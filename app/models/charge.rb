826adb6acb8af4b8f1f179ac9e72f22b33c8dfaf
jrkinouani <kinouani@gmail.com>
2015-07-06 22:48:08 +0400

    hotfix

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -3,8 +3,4 @@ class Charge < ActiveRecord::Base
 	validates :first_name, :presence => true
 	 validates :last_name, :presence => true
 	 validates :price, :presence => true
-
-	 def next_step
-  		self.current_step = steps[steps.index(current_step)+1]
-	end
 end
